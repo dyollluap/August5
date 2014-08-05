430b66105bca07c01c1441d3b17d695dd9d0b592
Paul Lloyd <plloyd@dn0a228608.sunet>
2014-08-05 11:00:51 -0700

    firstattempt

---
+++ primenumber1000.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a script to find prime numbers under 1000. 
+"""
+
+minIdx =1;
+maxIdx =1001;
+for i in range(minIdx,maxIdx):
+    isPrime = True;
+    for j in range(2,i):
+            if (i % j == 0):
+                isPrime = False;
+                break
+    if (isPrime == True):
+        print i
